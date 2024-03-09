503f7cd0bb2a2b693890e7b73934b3c5d2bc7b20
Zhuravlev Petr <zeterfrost@gmail.com>
2024-03-09 22:26:16 +0600

    added on message handler

--- api/router/drawings.ts
+++ api/router/drawings.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { ActiveConnections } from '../types';
+import {ActiveConnections, IPath} from '../types';
 
 const drawingsRouter = express.Router();
 const activeConnections: ActiveConnections = {};
@@ -7,8 +7,20 @@ const activeConnections: ActiveConnections = {};
 export const mountRouter = () => {
 	drawingsRouter.ws('/', (ws, req) => {
 		const id = crypto.randomUUID();
-		console.log(`client ${id} is connected`);
+		console.log(`client (${id}) is connected`);
 		activeConnections[id] = ws;
+
+
+		ws.on('message', (message) => {
+			const parsedDraw = JSON.parse(message.toString()) as IPath;
+			Object.values(activeConnections).forEach((connection) => {
+				if (activeConnections[id] !== connection) {
+					connection.send(JSON.stringify(parsedDraw));
+				}
+			});
+		});
+
+
 
 		ws.on('close', () => {
 			console.log(`client ${id} is disconnected!`);
--- api/types.d.ts
+++ api/types.d.ts
@@ -3,3 +3,13 @@ import { WebSocket } from 'ws';
 export interface ActiveConnections {
 	[id: string]: WebSocket;
 }
+
+export interface IMouseMove {
+	x: number;
+	y: number;
+}
+
+export interface IPath {
+	path: IMouseMove[];
+	color: string;
+}
