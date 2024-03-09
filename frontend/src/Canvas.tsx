40d4c543f4ba4f80b00e3bd85897fe7d07d8673f
Zhuravlev Petr <zeterfrost@gmail.com>
2024-03-09 22:42:26 +0600

    fixed bugs with updated component and refactor getDataDraw func

--- frontend/src/Canvas.tsx
+++ frontend/src/Canvas.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useCallback, useEffect, useRef } from 'react';
 import type { IMouseMove, IPath } from './types';
 
 interface Props {
@@ -11,77 +11,100 @@ interface Props {
 
 const Canvas: React.FC<Props> = ({width, height, getDraw , color, data}) => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
+  const drawPath = useRef<IPath>({path: [], color});
+  const isActive = useRef<boolean>(false);
+
+  const draw = useCallback(({x, y}: IMouseMove) => {
+    const canvas = canvasRef.current
+    if (!canvas) return;
+    const context = canvas.getContext('2d');
+    if (!context) return;
+
+    context.lineTo(x, y);
+    context.stroke();
+  }, []);
+
+  const handleMouseUp = useCallback(() => {
+    const canvas = canvasRef.current
+    if (!canvas) return;
+    const context = canvas.getContext('2d');
+    if (!context) return;
+    isActive.current = false;
+    context.stroke();
+    context.closePath();
+    getDraw(drawPath.current);
+    drawPath.current.path = [];
+  }, []);
+
+  const handleMouseMove = useCallback((e: MouseEvent) => {
+    const canvas = canvasRef.current
+    if (!canvas) return;
+    const context = canvas.getContext('2d');
+    if (!context) return;
+    if (isActive.current) {
+      const x = e.pageX - canvas.offsetLeft;
+      const y = e.pageY - canvas.offsetTop;
+      drawPath.current.path.push({x, y});
+      draw({x, y});
+    }
+  }, []);
+
+  const handleMouseDown = useCallback(() => {
+    const canvas = canvasRef.current
+    if (!canvas) return;
+    const context = canvas.getContext('2d');
+    if (!context) return;
+    isActive.current = true;
+    context.strokeStyle = color;
+    drawPath.current.color = color;
+    context.beginPath();
+    canvas.addEventListener('mousemove', handleMouseMove);
+    canvas.addEventListener('mouseup', handleMouseUp);
+  }, [color]);
+
+  const getDataDraw = useCallback(({path, color}: IPath) => {
+    const canvas = canvasRef.current
+    if (!canvas) return;
+    const context = canvas.getContext('2d');
+    if (!context) return;
+
+    context.beginPath();
+    context.strokeStyle = color;
+    context.moveTo(path[0].x, path[0].y);
+    path.forEach((point, index) => {
+      if (index > 0) {
+        context.lineTo(point.x, point.y);
+      }
+    });
+    context.stroke();
+    context.closePath();
+  }, [data]);
+
+
 
   useEffect(() => {
     const canvas = canvasRef.current;
     if (!canvas) return;
 
-    let isActive = false;
-    const path: IPath = {
-      path: [],
-      color
-    }
-
     const context = canvas.getContext('2d');
     if (!context) return;
 
+    canvas.addEventListener('mousedown', handleMouseDown);
+  }, [width, height, color]);
 
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    if (!canvas) return;
 
-    const draw = ({x, y}: IMouseMove) => {
-      context.lineTo(x, y);
-      context.stroke();
-    };
-
-    const getDataDraw = ({path, color}: IPath) => {
-      context.beginPath();
-      context.strokeStyle = color;
-      context.moveTo(path[0].x, path[0].y);
-      path.forEach((point, index) => {
-        if (index > 0) {
-          context.lineTo(point.x, point.y);
-        }
-      });
-      context.stroke();
-      context.closePath();
-    };
+    const context = canvas.getContext('2d');
+    if (!context) return;
 
     if (data) {
       data.forEach((item) => {
         getDataDraw(item);
       });
     }
-
-    const handleMouseDown = (e: MouseEvent) => {
-      isActive = true
-      const x = e.pageX - canvas.offsetLeft;
-      const y = e.pageY - canvas.offsetTop;
-      context.strokeStyle = color;
-      context.beginPath();
-      context.moveTo(x, y);
-      canvas.addEventListener('mousemove', handleMouseMove);
-
-
-      function handleMouseMove (e: MouseEvent) {
-        if (!canvas) return;
-        if (isActive) {
-          const x = e.pageX - canvas.offsetLeft;
-          const y = e.pageY - canvas.offsetTop;
-          path.path.push({x, y});
-          draw({x, y});
-        }
-      }
-
-      const handleMouseUp = () => {
-        isActive = false;
-        context.closePath();
-        getDraw(path);
-      };
-
-      canvas.addEventListener('mouseup', handleMouseUp);
-    };
-
-    canvas.addEventListener('mousedown', handleMouseDown);
-  }, [width, height, color]);
+  }, [data, getDataDraw]);
 
   return (
     <>
